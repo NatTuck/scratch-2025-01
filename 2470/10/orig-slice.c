13ec793407add34c1b8ebea28c6a3f6474be7d2a
Nat Tuck <nat@ferrus.net>
2025-03-10 11:00:03 -0400

    latest

--- 2470/10/orig-slice.c
+++ 2470/10/orig-slice.c
@@ -33,17 +33,6 @@ sfree(scell* xs)
     }
 }
 
-int
-slength(scell* xs)
-{
-    if (xs == 0) {
-        return 0;
-    }
-    else {
-        return 1 + slength(xs->next);
-    }
-}
-
 slice
 to_slice(const char* text)
 {
@@ -96,50 +85,10 @@ split_lines(const char* data, long sz)
         if (data[ii] == '\n') {
             item.len = cur_len;
             ys = scons(item, ys);
-            item.ptr = &(data[ii + 1]);
+            item.ptr = &(data[ii] + 1);
             cur_len = 0;
         }
-        else {
-            cur_len += 1;
-        }
-    }
-
-    return ys;
-}
-
-int
-slice_compare(const void* vaa, const void* vbb)
-{
-    slice* aa = (slice*) vaa;
-    slice* bb = (slice*) vbb;
-
-    char st0[aa->len + 1];
-    char st1[bb->len + 1];
-    memcpy(st0, aa->ptr, aa->len);
-    st0[aa->len] = 0;
-    memcpy(st1, bb->ptr, bb->len);
-    st1[bb->len] = 0;
-    return strcmp(st0, st1);
-}
-
-scell*
-sort_slices(scell* xs)
-{
-    int nn = slength(xs);
-    slice items[nn];
-
-    int ii = 0;
-    for (scell* it = xs; it; it = it->next) {
-        items[ii++] = it->item;
-    }
-
-    qsort(items, nn, sizeof(slice), slice_compare);
-
-    sfree(xs);
-
-    scell* ys = 0;
-    for (int ii = nn - 1; ii >= 0; --ii) {
-        ys = scons(items[ii], ys);
+        cur_len += 1;
     }
 
     return ys;
@@ -166,13 +115,9 @@ main(int argc, char* argv[])
 
     scell* lines = split_lines(data, sz);
 
-    lines = sort_slices(lines);
-
-    for (scell* it = lines; it; it = it->next) {
-        printf("%.*s\n", it->item.len, it->item.ptr);
+    for (scell* it = lines; lines; lines = lines->next) {
+        printf("%s\n", it->item.ptr);
     }
-
-    sfree(lines);
     
     free(data);
     return 0;
